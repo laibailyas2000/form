48a7bbd24ea49ec414945dd9ea27b908e0263f47
laibailyas2000 <laiba.ilyas00@gmail.com>
2024-06-10 15:53:51 +0500

    complete form code

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,103 @@
-function loginValidation() {
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForms = document.querySelectorAll("#login-form");
+  const signupForms = document.querySelectorAll("#signup-form");
+
+  // Event listeners for signup forms
+  signupForms.forEach(function (form) {
+    const signupBtn = form.querySelector("#signup-btn");
+    //const loginBtn = form.querySelector("#login-btn");
+
+    //signup btn
+    signupBtn.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent default action
+
+      const currentPage =
+        "file:///C:/Users/Blue%20Star%20Computer/Desktop/laiba/myFORM/public/html/signup.html"
+          .split("/")
+          .pop();
+
+      if (currentPage === "login.html") {
+        window.location.href = "signup.html";
+      } else if (currentPage === "signup.html" && signupValidation()) {
+        signupSubmission(event);
+      }
+    });
+  });
+
+  // Event listeners for login forms
+  loginForms.forEach(function (form) {
+    const signupBtn = form.querySelector("#signup-btn");
+    const loginBtn = form.querySelector("#login-btn");
+
+    //login btn
+    loginBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      const currentPage =
+        "file:///C:/Users/Blue%20Star%20Computer/Desktop/laiba/myFORM/public/html/login.html"
+          .split("/")
+          .pop();
+      if (currentPage === "signup.html") {
+        window.location.href = "login.html";
+      } else if (currentPage === "login.html") {
+        loginSubmission(event);
+      }
+    });
+  });
+});
+
+function loginSubmission(event) {
+  event.preventDefault();
+  const email = document.querySelector('input[name="email"]').value.trim();
+  const password = document.querySelector("input[name=psw]").value;
+
+  const loginData = {
+    email: email,
+    password: password,
+  };
+
+  fetchLoginData(loginData);
+}
+
+function fetchLoginData(loginData) {
+  fetch("http://localhost:1000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        window.alert("User not found");
+      } else if (!response.ok) {
+        console.log("erver side error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data from the login server: ", data.message);
+
+      if (data.message === "Login successful") {
+        {
+          const userData = {
+            user: data.user,
+            token: data.token,
+          };
+          console.log("User logged in : ", userData.user);
+          console.log("With a token of :", userData.token);
+        }
+        // Navigate to another page upon successful login
+        //window.location.href =
+        //        "file:///C:/Users/Blue%20Star%20Computer/Desktop/laiba/myFORM/public/html/form.html";
+      }
+      return data;
+    })
+    .catch((error) => {
+      console.error("Fetch error: ", error);
+    });
+}
+
+function signupValidation() {
   var userInput = document.querySelector('input[name="uname"]');
   var uname = userInput.value.trim();
   var isValid = true;
@@ -18,52 +117,58 @@ function loginValidation() {
   return isValid;
 }
 
-function handleFormSubmission(event) {
+function signupSubmission(event) {
   event.preventDefault(); // Prevent default form submission
 
-  if (!loginValidation()) {
+  if (!signupValidation()) {
     console.log("Form validation failed");
     return; // Exit function if validation fails
   }
 
   const uname = document.querySelector('input[name="uname"]').value.trim();
+  const email = document.querySelector('input[name="email"]').value.trim();
   const password = document.querySelector('input[name="psw"]').value;
 
-  const loginData = {
+  const signupData = {
     uname: uname,
+    email: email,
     password: password,
   };
+  fetchSignupData(signupData);
+}
 
-  fetch("http://localhost:1000/login", {
+function fetchSignupData(signupData) {
+  fetch("http://localhost:1000/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(loginData),
+    body: JSON.stringify(signupData),
   })
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Invalid credentials"); // or parse the response for specific error message
+      if (response.status === 400) {
+        window.alert("Email already exists");
+      } else if (!response.ok) {
+        console.log("Server side error");
       }
-      return response.json(); // Return the response body as JSON
+      return response.json();
     })
     .then((data) => {
-      console.log("Login response:", data.message); // Log the response message
-      // console.log("Result:", data.result); // Log the result
-      console.log("Inserted data:", data.result); // Log the inserted data
+      console.log("Data from server:", data.message);
 
-      if (data.message === "Login data inserted successfully") {
-        console.log("Login successful");
-        // Redirect or perform further actions upon successful login
+      return data;
+    })
+    .then((data) => {
+      console.log("data inserted through signup:", data.result);
+
+      if (data.message === "signup data inserted successfully") {
+        console.log("signup successful");
+        window.alert("signup successful");
       } else {
-        console.error("Login failed:", data.message); // Log the error message
+        console.error("signup failed"); // Log the error message
       }
     })
     .catch((error) => {
       console.error("Fetch error:", error);
     });
 }
-
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", handleFormSubmission);
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,45 +1,87 @@
 const { ObjectId } = require("mongodb");
 const { dbconnect, login } = require("../db/formDb-connect.js");
+const jwt = require("jsonwebtoken");
+const { secretKey } = require("../../public/js/token.js");
+// const secretKey = "bingo";
+
+async function signupForm(req, res) {
+  try {
+    const loginCollection = await login();
+    const { email, name, password } = req.body;
+    console.log("req body is ", req.body);
+
+    // Check if the email already exists
+    const existingUser = await loginCollection.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({ error: "email already exists" });
+    }
+
+    // Insert the signup data into the collection
+    const result = await loginCollection.insertOne({ email, name, password });
+
+    res.status(200).json({
+      message: "signup data inserted successfully",
+      result: result,
+    });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 
 async function loginForm(req, res) {
   try {
-    // Connect to the "loginValues" collection
     const loginCollection = await login();
+    const { email, password } = req.body;
 
-    // Extract login data from the request body
-    const { uname, password } = req.body;
-
-    // Construct the login document
-    const loginData = {
-      uname: uname,
+    // Check if the credentials match
+    const user = await loginCollection.findOne({
+      email: email,
       password: password,
-      // You can add more fields as needed
-    };
+    });
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
 
-    // Insert the login data into the collection
-    const result = await loginCollection.insertOne(loginData);
+    // // Generate JWT token
+    console.log("generate token");
+    const token = generateToken({ id: user._id, email: user.email });
 
-    // Send a response containing both the text and the result
     res.status(200).json({
-      message: "Login data inserted successfully",
-      result: result, // Send the database operation result
+      message: "Login successful",
+      user: user,
+      token: token,
     });
   } catch (error) {
-    // Handle errors
     console.error("Error during login:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-// async function loginForm(req, resp) {
-//   try {
-//     await login(); // Await the database connection
-//     console.log("data recived from user", req.body);
-//     resp.json({ message: "Login endpoint runned successful" });
-//   } catch (error) {
-//     console.error("Error during login:", error);
-//     resp.status(500).send("Internal Server Error");
+function generateToken(payload) {
+  return jwt.sign(payload, secretKey, { expiresIn: "10h" });
+}
+// // Protected endpoint
+// function Protected(verifyToken, req, res) {
+//   // If the token is verified, proceed with the protected operation
+//   verifyToken(req, res, () => {
+//     res.json({ message: "Protected resource accessed successfully" });
+//   });
+// }
+
+// // Middleware to verify JWT token
+// function verifyToken(req, resp, next) {
+//   const token = req.headers["authorization"];
+//   if (!token) {
+//     return resp.status(403).json({ message: "token is required" });
 //   }
+//   jwt.verify(token, secretKey, (err, decoded) => {
+//     if (err) {
+//       return resp.status(401).json({ message: "token in invalid" });
+//     }
+//     req.user = decoded; // Store decoded token data in request object
+//     next();
+//   });
 // }
 
 async function insertFormData(req, resp) {
@@ -78,9 +120,13 @@ async function updateFormData(req, res) {
 }
 
 module.exports = {
+  signupForm,
   loginForm,
   insertFormData,
   getFormData,
   deleteFormData,
   updateFormData,
+  generateToken,
 };
+
+//const { verifyToken, generateToken } = require("../../public/js/token.js");
